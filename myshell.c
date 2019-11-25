0164e3b93c15d09ca754a72d7c97bd236bf1a20f
honestus-codice <laser@email.arizona.edu>
2019-11-25 15:48:25 -0700

    wrote a function to count the number of things in the input array
    suck on that c

--- myshell.c
+++ myshell.c
@@ -11,20 +11,30 @@
 #define LEN 1024
 char* get_input();
 
-int main(int argc, char ** argv){
-    char * tokens = get_input();
-     while (tokens!= NULL){
-        printf("%s",tokens);
-        tokens = strtok(NULL,  " ()<>|&;");
-    }
-}
-
 char* get_input(){
     char buffer[LEN];
     fgets(buffer, LEN, stdin);
     char * tokens = strtok(buffer, " ()<>|&;");
     return tokens;
 }
+
+int count_tokens(char tokens[]){
+    int count = 0;
+    while (tokens!= NULL){
+        printf("%s ",tokens);
+        count++;
+        tokens = strtok(NULL,  " ()<>|&;");
+    }
+    return count;
+}
+
+int main(int argc, char ** argv){
+    char * tokens = get_input();
+    int count = count_tokens(&tokens);
+    printf("token count = %d ", count);
+}
+
+
 
 /**
  * The following exit, cd
