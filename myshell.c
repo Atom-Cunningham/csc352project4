5e215ab2470ff3058da869730f733893aa10c519
honestus-codice <laser@email.arizona.edu>
2019-11-25 11:16:50 -0700

    playing with syntax

---
+++ myshell.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+/**
+ * support exit
+ * cd
+ * ls, nano, pwd, ls -l,wc
+ * time
+ * 
+ */
+
+#define LEN 128
+int main(int argc, char ** argv){
+    get_input();
+}
+
+char* get_input(){
+    char buffer[LEN];
+    fgets(buffer, LEN, stdin);
+    printf("%s",strtok(buffer, " ()<>|&;"));
+}
+
+/**
+ * The following exit, cd
+ */
+
