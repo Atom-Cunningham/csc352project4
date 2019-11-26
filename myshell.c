3532226a7989ed28e31c39b0cbb250aef4055779
honestus-codice <laser@email.arizona.edu>
2019-11-25 17:52:09 -0700

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -30,7 +30,7 @@ int run(char * in){
     int arg_count;
     char * args[5];
     printf("running\n");
-    if (strcmp(args, EXIT) == 0){
+    if (strcmp(args[0], EXIT) == 0){
         printf("args[0]: %s\n", args[0]);
         exit(0);
     }
